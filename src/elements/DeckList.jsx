d70d2aa480be3d4bd657a1cec4e8e8874c999d5b
Vladimir <vladimir-nikolic@hotmail.com>
2023-09-09 19:11:40 +0200

    novi comit

--- src/elements/DeckList.jsx
+++ src/elements/DeckList.jsx
@@ -2,25 +2,19 @@ import React, { useState } from "react";
 import Card from "./Card";
 
 function DeckList({ deck, setDeck }) {
-
-  const activeStyle = {
-    backgroundColor: 'red'
-    
-  }
-  
   const handleClick = ({ name, selected, id }) => {
-
-    const deckUpdate = [...deck]
-    const updatedDeck = deckUpdate.map(card => (card.name === name && card.id === id) ?  {...card, selected:true} : card )
-    setDeck(updatedDeck)
+    const deckUpdate = [...deck];
+    const updatedDeck = deckUpdate.map((card) =>
+      card.name === name && card.id === id ? { ...card, selected: true } : card
+    );
+    setDeck(updatedDeck);
   };
   return (
-    <div className="parent" >
+    <div className="parent">
       {deck.map((card, index) => {
         const { id, name, selected, isMatched } = card;
         return (
           <Card
-            activeStyle={activeStyle}
             key={index}
             id={id}
             name={name}
